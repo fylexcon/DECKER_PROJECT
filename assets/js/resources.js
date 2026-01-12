66e9cbda10328f9a3ec28f927f64a1e794a1adc5
fylexcon <s.suna604@gmail.com>
2026-01-12 23:54:43 +0300

    added resource page

---
+++ assets/js/resources.js
@@ -0,0 +1,7 @@
+/* --- assets/js/resources.js --- */
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Sayfaya özel scriptler buraya eklenebilir.
+  // Şimdilik animasyonlar main.js üzerindeki 'reveal' sınıfı ile çalışıyor.
+  console.log("Resources page loaded.");
+});
